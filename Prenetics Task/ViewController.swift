b36083408ccd36efc1270fc856397ee0165d9959
Kishore <kishore.prakash@redcross.org>
2018-11-22 15:06:40 +0530

    Initial Commit

---
+++ Prenetics Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prenetics Task
+//
+//  Created by Kishore on 22/11/18.
+//  Copyright © 2018 Kishore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
